3751a63942cc7a18e59313cdfb895a2fe179c936
Inka Web <99542861+AryuInka@users.noreply.github.com>
2022-05-22 10:46:47 +0200

    Update Program.cs

--- Valorant-Aimbot/AIMBOT/Program.cs
+++ Valorant-Aimbot/AIMBOT/Program.cs
@@ -9,7 +9,7 @@ namespace Immortal
 	// Token: 0x02000004 RID: 4
 	internal static class Program
 	{
-		// Token: 0x0600001E RID: 30 RVA: 0x00004BFC File Offset: 0x00002DFC
+		// Token: 0x0600001E RID: 30 RVA: 0x00004BFC File Offset: 0x00002DFCdqz
 		[STAThread]
 		private static void Main(string[] args)
 		{s
